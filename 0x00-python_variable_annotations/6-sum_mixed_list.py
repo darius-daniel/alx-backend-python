bfe2a68a2b77c3356783cdcf1b8f478629d2b29e
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-05 15:12:39 +0100

    Replaced Union shorthand operand with comma (,) in parameter annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Contains a function that takes a mixed list and returns their sum as a float
 import typing
 
 
-def sum_mixed_list(mxd_list: typing.List[typing.Union[float | int]]) -> float:
+def sum_mixed_list(mxd_list: typing.List[typing.Union[float, int]]) -> float:
     """
     Takes in a list of mixed values (ints and floats) and returns their float
 
