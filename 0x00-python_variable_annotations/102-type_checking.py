5f447680c6d534c1bcfbc5b5f5024aa95c2100ed
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-05 17:26:13 +0100

    Added docstring to function zoom_array in 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,6 +6,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """To demonstrate the usage of mypy to validate a piece of code"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
