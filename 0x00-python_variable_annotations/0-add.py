eb3bf4ff0bc9d162382670420862e76027f58046
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-05 14:53:34 +0100

    Made function docstring more descriptive

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,14 @@
 #!/user/bin/env python3
-"""Contains a function that returns the sum of two floating point numbers."""
+"""
+Contains a function that returns the sum of two floating point numbers.
+"""
 
 
 def add(a: float, b: float) -> float:
-    """Takes 2 floats as arguments and returns their sum"""
+    """
+    Takes 2 floats as arguments and returns their sum
+    :param a: first floating point number
+    :param b: second floating point number
+    :return: sum of @a and @b
+    """
     return a + b
