68d23cc45cd8f7553c7b0898432e7d05f892bf37
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-09 15:01:37 +0100

    Adding script for task 3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""""""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Future:
+    """
+    Takes an integer max_delay and returns a asyncio.Task.
+    :param max_delay: maximum function delay time
+    :return: an asyncio.Task object
+    """
+    return asyncio.create_task(wait_random(max_delay))
