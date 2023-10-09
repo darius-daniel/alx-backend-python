1ec5a81820e3e8732125dd7b8db4cbfb79dd10d9
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-09 13:34:04 +0100

    Modified function to return list of wait times arranged in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,22 +2,27 @@
 """
 1. Execute multiple coroutines at the same time with async
 """
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """
-
-    :param n:
-    :param max_delay:
-    :return:
+    Spawns wait_random n times with the specified max_delay
+    :param n: number of times wait_random() is spawned
+    :param max_delay: the maximum sleep length of asyncio.sleep()
+    :return: a list of floats
     """
-    import asyncio
-
     delays: list[float] = []
 
     for i in range(n):
-        delays.append(await wait_random(max_delay))
+        delay: float = await wait_random(max_delay)
+
+        i: int = 0
+        while i < len(delays):
+            if delays[i] >= delay:
+                break
+            i += 1
+
+        delays.insert(i, delay)
 
     return delays
