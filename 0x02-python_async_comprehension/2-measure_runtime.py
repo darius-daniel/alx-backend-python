e391b26179507d4ffb27b5527631bac969014b73
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-10 14:00:19 +0100

    Refactored code in 2-measure_runtime.py by removing unused function call async_comprehension()

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,6 @@
 """2. Run time for four parallel comprehensions"""
 import time
 import asyncio
-from typing import AsyncGenerator
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -15,20 +14,7 @@ async def measure_runtime() -> float:
     parallel
     """
     start: float = time.time()
-    await asyncio.gather(
-        *[async_comprehension() async for num in number(4)])
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end: float = time.time()
 
     return end - start
-
-
-async def number(times: int) -> AsyncGenerator[int, None]:
-    """
-    Yields async_comprehension @times times
-    :param times: number of times async_comprehension is yielded by the
-        function
-    :return: Nothing
-    """
-    for i in range(times):
-        yield i
-        await asyncio.sleep(1)
