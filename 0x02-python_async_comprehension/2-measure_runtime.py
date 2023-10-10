b263cf66ade3bf747b11be900ca8000d2589ef60
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-10 12:17:32 +0100

    Modified yield type annotation in 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,16 +2,15 @@
 """2. Run time for four parallel comprehensions"""
 import time
 import asyncio
-from collections.abc import Callable
-from typing import AsyncGenerator, List
+from typing import Callable, Generator, List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    Executes async_comprehension four times in parallel using asyncio.gather(),
-    and measures the total runtime and returns it.
+    Executes async_comprehension four times in parallel using
+    asyncio.gather(), and measures the total runtime and returns it.
     :return: the total runtime of executing async_comprehension four time in
     parallel
     """
@@ -24,7 +23,7 @@ async def measure_runtime() -> float:
 
 
 async def call_async_comprehension(
-        times: int) -> AsyncGenerator[Callable[[], List[int]], int]:
+        times: int) -> Generator[Callable[[], List[int]], None, None]:
     """
     Yields async_comprehension @times times
     :param times: number of times async_comprehension is yielded by the
