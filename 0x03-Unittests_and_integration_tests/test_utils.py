accc5fff62381323607d6cb6e227543868baa63c
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-30 14:07:59 +0100

    Commiting modifications for task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,9 @@
 """ 0. Parameterize a unit test
 """
 import unittest
-import utils
 from parameterized import parameterized
-from utils import access_nested_map, get_json
-from unittest.mock import patch, Mock, MagicMock
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
 from typing import Dict, Sequence, Union, Type
 
 
@@ -46,12 +45,34 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    def test_get_json(self, url: str, payload: Dict):
+    def test_get_json(self, url: str, payload: Dict) -> None:
         """Tests the output of the function get_json."""
         attrs = {'json.return_value': payload}
         with patch("requests.get", return_value=Mock(**attrs)) as fetched:
             self.assertEqual(get_json(url), payload)
             fetched.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Suite of tests for the memoize function"""
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+        ) as fn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            fn.assert_called_once()
 
 
 if __name__ == '__main__':
